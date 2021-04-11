3cde50f3ffc4685aeb20b5041bf668ec3a45a453
Dakota <>
2021-04-11 22:45:31 +0000

    Fixed various bugs resulting in issues displaying final game statement after 3rd round.
    General statement/aesthetic updates in terms of messaging

--- Game.java
+++ Game.java
@@ -47,30 +47,28 @@ public class Game {
 
 		//Logic for multiple rounds - first player to 2 rounds won wins the entire game, max number of rounds possible in one game is 3, will break out of outer loop when a player
 		// gets to 2 rounds won.
-		for(int j = 1; j <= 3; j++) {
+		int totalRounds = 1; //used to keep track of all rounds, to incorporate tie logic, j is used for best of 3 mechanic, refactor this to be more concise in future
+		for(int j = 1; j <= 4; j++) {
 
 			//to create a new shuffled deck for each round (since deck already created for first round, only does this round 2 and on)
 			// Need to refactor this
-			if(j > 1) {
-				deck.createAndShuffleDeck();
-				players[0].setPlayerDeck(0, deck);
-				players[1].setPlayerDeck(26, deck);
-				playerOneDeck = players[0].getPlayerDeck();
-				playerTwoDeck = players[1].getPlayerDeck();
-			}
 
-			if(players[0].getRoundsWon() == 2) {
-				System.out.println();
-				System.out.println(players[0].getName() + " won the game with a total of 2 rounds to " + players[1].getRoundsWon() + "!");
-				//break statement to account for the 2-0 case, so a third game isn't played unnecessarily, but if on third round, it would exit loop regardless
-				break;
-			} else if(players[1].getRoundsWon() == 2) {
-				System.out.println();
-				System.out.println(players[1].getName() + " won the game with a total of 2 rounds to " + players[0].getRoundsWon() + "!");
-				//break statement to account for the 2-0 case, so a third game isn't played unnecessarily, but if on third round, it would exit loop regardless
-				break;
-			} else {
+
+			
+			if(players[0].getRoundsWon() < 2 && players[1].getRoundsWon() < 2) {
+				
+				if(totalRounds > 1 && totalRounds <= 4) {
+					System.out.println();
+					System.out.println("Press Enter to Continue to the Next Round!");
+					deck.createAndShuffleDeck();
+					players[0].setPlayerDeck(0, deck);
+					players[1].setPlayerDeck(26, deck);
+					playerOneDeck = players[0].getPlayerDeck();
+					playerTwoDeck = players[1].getPlayerDeck();
+				}
+
 				for(int i = 0; i < 26; i++) {
+
 					//this nextLine() is to force the loop to only iterate when the user presses the Enter key, to see a step by step version of the handsoff game
 					scanner.nextLine();
 
@@ -95,26 +93,37 @@ public class Game {
 				System.out.println("Press Enter to see Round Summary!");
 				scanner.nextLine();
 
-				if(players[0].getRoundScore() > players[1].getRoundScore()) {
-					System.out.println(players[0].getName() + " is the winner of the round with a total of " + players[0].getRoundScore() + " points!");
-					players[0].setRoundsWon(players[0].getRoundsWon() + 1);
-				} else if(players[0].getRoundScore() < players[1].getRoundScore()) {
-					System.out.println(players[1].getName() + " is the winner of the round with a total of " + players[1].getRoundScore() + " points!");
-					players[1].setRoundsWon(players[1].getRoundsWon() + 1);
-				} else {
-					System.out.println("It was a tie! No-one receives a point for this round.");
-					j--;
-				}
-				players[0].setRoundScore(0);
-				players[1].setRoundScore(0);
+			} else if(players[0].getRoundsWon() == 2) {
+				System.out.println();
+				System.out.println(players[0].getName() + " won the game with a total of 2 rounds to " + players[1].getRoundsWon() + "!");
+				//break statement to account for the 2-0 case, so a third game isn't played unnecessarily, but if on third round, it would exit loop regardless
+				break;
+			} else if(players[1].getRoundsWon() == 2) {
+				System.out.println();
+				System.out.println(players[1].getName() + " won the game with a total of 2 rounds to " + players[0].getRoundsWon() + "!");
+				//break statement to account for the 2-0 case, so a third game isn't played unnecessarily, but if on third round, it would exit loop regardless
+				break;
 			}
+
+			if(players[0].getRoundScore() > players[1].getRoundScore()) {
+				System.out.println(players[0].getName() + " is the winner of the round with a total of " + players[0].getRoundScore() + " points!");
+				players[0].setRoundsWon(players[0].getRoundsWon() + 1);
+			} else if(players[0].getRoundScore() < players[1].getRoundScore()) {
+				System.out.println(players[1].getName() + " is the winner of the round with a total of " + players[1].getRoundScore() + " points!");
+				players[1].setRoundsWon(players[1].getRoundsWon() + 1);
+			} else {
+				System.out.println("It was a tie! No-one receives a point for this round.");
+				j--;
+			}
+			players[0].setRoundScore(0);
+			players[1].setRoundScore(0);
 
 			System.out.println();
 			System.out.println("The current round score is " + players[0].getName() + ": " + players[0].getRoundsWon() + " and " + players[1].getName() + ": " + players[1].getRoundsWon());
 
-		}
+			totalRounds++;
 
-		System.out.println("Press ENTER to continue!");
+		}
 
 	scanner.close();
 	}
